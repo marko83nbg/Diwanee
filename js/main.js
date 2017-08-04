72970e9a578ce4307493374de09528223df65062
Marko Tomasevic <marko83nbg@gmail.com>
2017-08-04 17:27:42 +0200

    nav

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$(document).ready(function(){
+    
+    $(".nav-toggle").click(function(){
+        
+        $(".mobil-nav").slideToggle(1000);
+        
+    });
+    
+    
+    
+    
+    
+});
 
